456c794af48317131b56de7a67bec335033db401
aboytech <francisco@aboytech.com>
2024-09-05 18:05:34 -0600

    Create plot_hist helper function

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 # Function to retrieve Conservation Status of Species in a new dataframe
 def get_species_conservation_status(dataframe, conservation_status):
 
@@ -9,7 +13,7 @@ def get_aggregates(dataframe, column_name, column_formal, type):
 
     mean = dataframe[column_name].mean()
     median = dataframe[column_name].median()
-    mode = dataframe[column_name].mode()
+    mode = dataframe[column_name].mode()[0]
     min = dataframe[column_name].min()
     max = dataframe[column_name].max()
 
@@ -23,3 +27,26 @@ def get_aggregates(dataframe, column_name, column_formal, type):
 
     return mean, median, mode, min, max
 
+# Plot an histogram to see distributions
+def plot_hist(dataframe, column_name, aggregates, xlabel, ylabel, title):
+
+    counts, bins = np.histogram(dataframe[column_name])
+
+    count = 0
+
+    count = 0
+    for index in counts:
+        if count < index:
+            count = index
+
+
+    plt.hist(dataframe[column_name])
+    plt.vlines(x = aggregates[0], ymin = 0, ymax = count, color = 'red', linestyles = 'dotted')
+    plt.vlines(x = aggregates[1], ymin = 0, ymax = count, color = 'green', linestyles = 'dotted')
+    plt.vlines(x = aggregates[2], ymin = 0, ymax = count, color = 'black', linestyles = 'dotted')
+    plt.legend(['Mean', 'Median', 'Mode'])
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+    plt.clf()
